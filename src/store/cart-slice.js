1ba753185916474f5da3dca84bddc072ffa64aa3
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-04-05 01:53:38 +0530

    fix: cart items total price update issue

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -26,6 +26,7 @@ const cartSlice = createSlice({
             }
             else {
                 existingItem.quantity--
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price
             }
         }
     }
