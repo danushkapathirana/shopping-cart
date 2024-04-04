bfd36789dfdcce4cb9f0085811e496d327df9730
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-04-05 00:09:17 +0530

    develop all initial components of shopping cart application

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Card from "../UI/Card"
+import CartItem from "./CartItem"
+
+import classes from "./Cart.module.css"
+
+const Cart = () => {
+    return(
+        <Card className={classes.cart}>
+            <h2>Your Shopping Cart</h2>
+
+            <ul>
+                <CartItem item={{title: "Test Item", quantity: 3, total: 18, price: 6}} />
+            </ul>
+        </Card>
+    )
+}
+
+export default Cart
