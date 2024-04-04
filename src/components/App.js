9e7b9adaebc63a43d98b50090bf29e804c732339
Danushka Pathirana <119834779+danushkapathirana@users.noreply.github.com>
2024-04-05 00:38:17 +0530

    develop to toggle cart ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react"
+import { useSelector } from "react-redux"
 
 import Layout from "./Layout/Layout"
 import Cart from "./Cart/Cart"
 import Products from "./Shop/Products"
 
 const App = () => {
+    const showCart = useSelector((state) => state.ui.cartIsVisible)
+    
     return(
         <Layout>
-            <Cart />
+            {showCart && <Cart />}
             <Products />
         </Layout>
     )
